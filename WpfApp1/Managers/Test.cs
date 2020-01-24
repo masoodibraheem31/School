43ce958e6f000218beafd321c957f60ce277420d
masoodibraheem31 <masoodahmad782@gmail.com>
2020-01-25 01:52:22 +0530

    add new prop

--- WpfApp1/Managers/Test.cs
+++ WpfApp1/Managers/Test.cs
@@ -10,5 +10,7 @@ namespace WpfApp1.Managers
         public string name { get; set; }
         public string email { get; set; }
         public string phone { get; set; }
+        public string address { get; set; }
+        public string gender { get; set; }
     }
 }
